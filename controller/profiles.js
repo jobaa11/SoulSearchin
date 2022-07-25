b1f323b0a6178fc49d266ceb9e5e5d817c167b4b
username <youremail@domain.com>
2022-07-25 15:05:29 -0700

    updated create function

--- controller/profiles.js
+++ controller/profiles.js
@@ -1,9 +1,18 @@
 const Profile = require('../models/profile');
 
 module.exports = {
-    new: newProfile
+    new: newProfile,
+    create
 }
 
 function newProfile(req, res) {
     res.render('profiles/new');
+}
+
+function create(req, res) {
+    var profile = new Profile
+    profile.save(function (err) {
+        if (err) return res.redirect('/profiles/new');
+        // res.redirect('/profiles');
+    })
 }
--- models/instrument.js
+++ models/instrument.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const instrumentScheme = new Schema({
-    
+const instrumentSchema = new Schema({
+    name: {type: String, enum: ['Piano', 'Guitar', 'Percussion', 'Bass', 'Saxophone', 'Trumpet']}
 })
+
+module.exports = mongoose.model('Instrument', instrumentSchema);
