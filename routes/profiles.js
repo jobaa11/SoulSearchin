f99b37626b560895a93c635de174aa57d550d3aa
username <youremail@domain.com>
2022-07-25 12:52:31 -0700

    added create functionality for profiles

--- routes/profiles.js
+++ routes/profiles.js
@@ -5,5 +5,9 @@ const profilesCtrl = require('../controller/profiles')
 
 // GET /profiles/new (/new)
 router.get('/new', profilesCtrl.new);
+// POST /profiles/instructor - create functionality
+router.post('/instructor', profilesCtrl.create);
+// POST /profiles/student - create functionality
+router.post('/student', profilesCtrl.create);
 
 module.exports = router;
