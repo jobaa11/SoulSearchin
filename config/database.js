a5d20baa0f867dae70478b9b71c21e94aa82b904
username <youremail@domain.com>
2022-10-23 22:54:40 -0700

    added value attri

--- config/database.js
+++ config/database.js
@@ -1,15 +1,16 @@
 const mongoose = require('mongoose');
 
-async () => {
-    try {
-        await mongoose.connect(process.env.DATABASE_URL);
-        const db = await mongoose.connection;
+// async () => {
+    // try {
+         mongoose.connect(process.env.DATABASE_URL);
+        // await mongoose.connect(process.env.DATABASE_URL);
+        const db =  mongoose.connection;
         db.on('connected', function () {
             console.log(`Connected to ${db.name} MongoDB at ${db.host}:${db.port}`);
         });
-    } catch (err) {
-        console.error(err)
-    }
-}
+//     } catch (err) {
+//         console.error(err)
+//     }
+// }
 
 
