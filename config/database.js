0aa3ec54ec8cf93aafd8f0a12028ed58aac4840d
username <youremail@domain.com>
2022-10-23 23:12:38 -0700

    refactored database async, iife

--- config/database.js
+++ config/database.js
@@ -1,16 +1,15 @@
 const mongoose = require('mongoose');
 
-// async () => {
-    // try {
-         mongoose.connect(process.env.DATABASE_URL);
-        // await mongoose.connect(process.env.DATABASE_URL);
-        const db =  mongoose.connection;
+(async () => {
+    try {
+        await mongoose.connect(process.env.DATABASE_URL);
+        const db = await mongoose.connection;
         db.on('connected', function () {
             console.log(`Connected to ${db.name} MongoDB at ${db.host}:${db.port}`);
         });
-//     } catch (err) {
-//         console.error(err)
-//     }
-// }
+    } catch (err) {
+        console.error(err)
+    }
+})();
 
 
